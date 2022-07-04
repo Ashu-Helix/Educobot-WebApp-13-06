db44ee9cdde83d7cbbde5f18b158d4859ae4df04
ramila <ramila.choudhary@helixstack.in>
2022-07-04 11:50:51 +0530

    help pop changed

--- components/helpers/openModel.js
+++ components/helpers/openModel.js
@@ -1,5 +1,7 @@
 let helpCode = () => { $('#help_practice').modal('open'); };
 $('document').ready(function () {
     $('#help_practice').modal();
+    $('#help_practice').modal('open');
+
 });
 export { helpCode }
